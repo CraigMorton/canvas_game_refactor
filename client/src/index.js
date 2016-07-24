f1636fd8e150373dd92520d5e82fb14bd68d4249
Craig Morton <CraigMorton22@gmail.com>
2016-07-24 17:54:30 +0100

    refactored var to const / let

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,8 @@ import Sprite from "./sprite.js";
 
 window.onload = function(){
   // Create the canvas
-  var canvas = document.createElement("canvas");
-  var ctx = canvas.getContext("2d");
+  const canvas = document.createElement("canvas");
+  const ctx = canvas.getContext("2d");
   canvas.width = 512;
   canvas.height = 480;
   document.body.appendChild(canvas);
@@ -17,14 +17,14 @@ window.onload = function(){
   const monsterImage = new Sprite(imageUrls.monster);
 
   // Game objects
-  var hero = {
+  const hero = {
     speed: 256 // movement in pixels per second
   };
-  var monster = {};
-  var monstersCaught = 0;
+  const monster = {};
+  let monstersCaught = 0;
 
   // Handle keyboard controls
-  var keysDown = {};
+  const keysDown = {};
 
   addEventListener("keydown", function (e) {
     keysDown[e.keyCode] = true;
@@ -35,7 +35,7 @@ window.onload = function(){
   }, false);
 
   // Reset the game when the player catches a monster
-  var reset = function () {
+  const reset = function () {
     hero.x = canvas.width / 2;
     hero.y = canvas.height / 2;
 
@@ -45,7 +45,7 @@ window.onload = function(){
   };
 
   // Update game objects
-  var update = function (modifier) {
+  const update = function (modifier) {
     if (38 in keysDown) { // Player holding up
       hero.y -= hero.speed * modifier;
     }
@@ -72,7 +72,7 @@ window.onload = function(){
   };
 
   // Draw everything
-  var render = function () {
+  const render = function () {
     ctx.drawImage(bgImage, 0, 0);
     ctx.drawImage(heroImage, hero.x, hero.y);
     ctx.drawImage(monsterImage, monster.x, monster.y);
@@ -86,9 +86,9 @@ window.onload = function(){
   };
 
   // The main game loop
-  var main = function () {
-    var now = Date.now();
-    var delta = now - then;
+  const main = function () {
+    const now = Date.now();
+    const delta = now - then;
 
     update(delta / 1000);
     render();
@@ -100,11 +100,11 @@ window.onload = function(){
   };
 
   // Cross-browser support for requestAnimationFrame
-  var w = window;
+  const w = window;
   requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
 
   // Let's play this game!
-  var then = Date.now();
+  const then = Date.now();
   reset();
   main();
 };
